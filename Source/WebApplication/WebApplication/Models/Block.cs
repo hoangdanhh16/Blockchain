61630ea8d90a8b8ea043cb9db31051f84792c816
hoangdanhh16 <hoangdanhh16@gmail.com>
2022-04-28 23:26:56 +0700

    add models(block, blockchain, transaction)

---
+++ Source/WebApplication/WebApplication/Models/Block.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Block
+    {
+        private readonly DateTime _timeStamp;
+        private long _nonce;
+        public string PreviousHash { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public string Hash { get; set; }
+
+        public Block(DateTime timeStamp, List<Transaction> Transactions, string prevHash = "")
+        {
+            _timeStamp = timeStamp;
+            _nonce = 0;
+            this.Transactions = Transactions;
+            PreviousHash = prevHash;
+            Hash = CreateHash();
+        }
+
+        public void MineBlock(int proofOfWorkDifficulty)
+        {
+            string hashValidationTemplate = new String('0', proofOfWorkDifficulty);
+            while (Hash.Substring(0, proofOfWorkDifficulty) != hashValidationTemplate)
+            {
+                _nonce++;
+                Hash = CreateHash();
+            }
+        }
+
+        public string CreateHash()
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                string rawData = PreviousHash + _timeStamp + Transactions + _nonce;
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                return Encoding.Default.GetString(bytes);
+            }
+        }
+    }
+}
---
+++ Source/WebApplication/WebApplication/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Transaction
+    {
+        public string From { get; }
+        public string To { get; }
+        public double Amount { get; }
+        public Transaction(string from, string to, double amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
